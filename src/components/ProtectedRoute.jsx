e01b3c9945172e9075fc415c3b0053e214ba6541
Vivek kumar <vivekkumar031096@gmail.com>
2024-04-09 16:13:11 +0530

    home page nav bar

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { getCurrentUser } from '../apiCalls/users'
 import {  useNavigate } from 'react-router-dom';
-import {message} from 'antd';
+import {message , Layout , Menu} from 'antd';
 import { hideLoading, showLoading } from '../redux/loaderSlice';
 import {useDispatch , useSelector} from 'react-redux';
+import { Header } from "antd/es/layout/layout";
+import {HomeOutlined , UserOutlined } from '@ant-design/icons'
 
 const ProtectedRoute = ({children}) => {
+ 
+    const navItems = [
+        {
+            label:'Home',
+            icon:<HomeOutlined />
+        },
+        {
+            label:'Profile',
+            icon:<UserOutlined />
+        }
+    ];
+
     const navigate = useNavigate();
     const [user,setUser] = useState(null);
     const dispatch = useDispatch();
@@ -36,7 +50,26 @@ const ProtectedRoute = ({children}) => {
         
     },[]);
   return (
-    <div>{user && user.name}{children}</div>
+    // <div>{user && user.name}{children}</div>
+    <>
+    <Layout>
+        <Header
+        className="d-flex justify-content-between"
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 1,
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+        }}
+        >
+            <h3 className="demo-logo text-white m-0" style={{color:'white'}}>Book My Show</h3>
+
+            <Menu theme='dark' mode='horizontal' items={navItems} />
+        </Header>
+    </Layout>
+    </>
   )
 }
 
