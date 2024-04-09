8785646f70286874b930ae72936e32f13af99040
Vivek kumar <vivekkumar031096@gmail.com>
2024-04-09 15:33:00 +0530

    redux and loader

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ProtectedRoute from './components/ProtectedRoute';
+import {useSelector} from 'react-redux';
 
 
 function App() {
+
+  const {loading} = useSelector((state) => state.loader)
   
   return (
     <div className="App">
+      {loading && <div className='loader-container'> <div className="loader"></div> </div>}
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<ProtectedRoute> <Home /> </ProtectedRoute>} />
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,17 +2,25 @@ import React, { useEffect, useState } from 'react'
 import { getCurrentUser } from '../apiCalls/users'
 import {  useNavigate } from 'react-router-dom';
 import {message} from 'antd';
+import { hideLoading, showLoading } from '../redux/loaderSlice';
+import {useDispatch , useSelector} from 'react-redux';
 
 const ProtectedRoute = ({children}) => {
     const navigate = useNavigate();
     const [user,setUser] = useState(null);
+    const dispatch = useDispatch();
+
     
     const getValidUser = async () =>{
         try {
+            //show loader
+            dispatch(showLoading());
             const response =await getCurrentUser();
             console.log(response);
             setUser(response.data);
-            
+
+            //hide loader
+            dispatch(hideLoading());
         } catch (error) {
             setUser(null);
             message.error(error.message);
